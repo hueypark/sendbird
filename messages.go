6ff792755e140685eca99487ffb2fdb3306bfef1
Jaewan Park <jaewan.huey.park@gmail.com>
2020-06-21 14:56:53 +0900

    Add the ability to specify type to read messages

--- messages.go
+++ messages.go
@@ -14,19 +14,31 @@ type Message struct {
 	MessageID int64  `json:"message_id"`
 }
 
+// MessageReadType is typeof message read.(timestamp or id)
+type MessageReadType int
+
+const (
+	MessageReadID MessageReadType = iota
+	MessageReadTimestamp
+)
+
 // Messages returns messages in channel
 func (sb *Sendbird) Messages(
-	channelType string, channelURL string, messageID int64,
+	channelType string,
+	channelURL string,
+	messageReadType MessageReadType,
+	messageReadVal int64,
 ) ([]Message, error) {
-	resp, err := sb.Request(
-		http.MethodGet,
-		fmt.Sprintf(
-			"/%s/%s/messages?message_id=%v",
-			channelType,
-			channelURL,
-			messageID,
-		),
-		nil)
+	url := fmt.Sprintf("/%s/%s/messages?", channelType, channelURL)
+
+	switch messageReadType {
+	case MessageReadID:
+		url += fmt.Sprintf("message_id=%v", messageReadVal)
+	case MessageReadTimestamp:
+		url += fmt.Sprintf("message_ts=%v", messageReadVal)
+	}
+
+	resp, err := sb.Request(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
