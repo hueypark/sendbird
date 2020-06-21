6127071658e0421336baf2b7ee2bccf8372ea01f
Jaewan Park <jaewan.huey.park@gmail.com>
2020-06-21 17:51:07 +0900

    Add message chat with random user

--- examples/client/sendbird.go
+++ examples/client/sendbird.go
@@ -1,16 +1,70 @@
 package main
 
-import "github.com/maxence-charriere/go-app/v6/pkg/app"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/hueypark/sendbird"
+	"github.com/maxence-charriere/go-app/v6/pkg/app"
+)
+
+var (
+	applicationID string
+	apiToken      string
+
+	// channelURL for tempolary test.
+	channelURL = "sendbird"
+
+	// users for tempolary test.
+	users = []sendbird.User{
+		{
+			UserID:   "charles",
+			Nickname: "charles",
+		},
+		{
+			UserID:   "david",
+			Nickname: "david",
+		},
+		{
+			UserID:   "hueypark",
+			Nickname: "hueypark",
+		},
+		{
+			UserID:   "jay",
+			Nickname: "jay",
+		},
+		{
+			UserID:   "john",
+			Nickname: "john",
+		},
+	}
+)
 
 type frontend struct {
 	app.Compo
-	messages []message
+	sb            *sendbird.Sendbird
+	user          *sendbird.User
+	lastMessageID int64
+	messages      []sendbird.Message
 }
 
-type message struct {
-	userID       int
-	userNickname string
-	message      string
+func newFrontend() (*frontend, error) {
+	f := &frontend{}
+
+	var err error
+	f.user, err = getRandomUser()
+	if err != nil {
+		return nil, err
+	}
+
+	f.sb, err = sendbird.NewSendbird(applicationID, apiToken)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
 
 func (f *frontend) Render() app.UI {
@@ -21,13 +75,40 @@ func (f *frontend) Render() app.UI {
 		app.H1().Body(app.Text("Sendbird Go Client")),
 	)
 
+	log.Println(f.messages)
+
+	var messages []sendbird.Message
+	var err error
+	if f.lastMessageID == 0 {
+		f.messages, err = f.sb.Messages(
+			sendbird.OpenChannels,
+			channelURL,
+			sendbird.MessageReadTimestamp,
+			time.Now().Unix()*1000,
+		)
+	} else {
+		messages, err = f.sb.Messages(
+			sendbird.OpenChannels,
+			channelURL,
+			sendbird.MessageReadID,
+			f.lastMessageID,
+		)
+	}
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	f.updateLastMessageID(messages)
+
+	f.messages = append(f.messages, messages...)
+
 	for _, message := range f.messages {
 		nodes = append(
 			nodes,
 			app.Div().Body(
-				app.Span().Text(message.userNickname),
+				app.Span().Text(message.User.Nickname),
 				app.Span().Text(": "),
-				app.Span().Text(message.message),
+				app.Span().Text(message.Message),
 			),
 		)
 	}
@@ -47,6 +128,34 @@ func (f *frontend) Render() app.UI {
 }
 
 func (f *frontend) OnChat(src app.Value, e app.Event) {
-	//f.name = src.Get("value").String()
+	message := src.Get("value").String()
+
+	_, err := f.sb.SendMessage(sendbird.OpenChannels, channelURL, f.user.UserID, message)
+	if err != nil {
+		log.Println(err)
+	}
+
 	f.Update()
+}
+
+func (f *frontend) updateLastMessageID(messages []sendbird.Message) {
+	for _, message := range messages {
+		if f.lastMessageID <= message.MessageID {
+			f.lastMessageID = message.MessageID
+		}
+	}
+}
+
+func getRandomUser() (*sendbird.User, error) {
+	rand.Seed(time.Now().UnixNano())
+	idx := rand.Intn(len(users))
+	for i, user := range users {
+		if i != idx {
+			continue
+		}
+
+		return &user, nil
+	}
+
+	return nil, fmt.Errorf("can not get random user")
 }
